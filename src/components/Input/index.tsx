52def8545453d9c9402fbd6bebbda7e97129b140
João Azevedo <joao.azevedo@ufv.br>
2021-07-28 21:45:27 -0300

    Header and Input components are finished

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps {
+  name: string,
+  icon: any,
+}
+
+function Input ({ name, icon: Icon, ...rest }:IInputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
